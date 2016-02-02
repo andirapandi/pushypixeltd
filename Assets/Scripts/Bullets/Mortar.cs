a4c8243222cdab62ce0e3bfbe07662804f486629
andi <unitygit@andreas-reiff.de>
2016-02-02 23:46:37 +0100

    explosions added, still need to have an impact, but have animation already (44 min into video 5)

---
+++ Assets/Scripts/Bullets/Mortar.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mortar : MonoBehaviour
+{
+    public GameObject explosion;
+
+    public float explosionDuration = .4f;
+    public float initialScale = 0.5f;
+    public float endScale = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    GameObject curExplosion;
+    void OnCollisionEnter(Collision collision)
+    {
+        curExplosion = Instantiate(explosion, collision.contacts[0].point, Quaternion.identity) as GameObject;
+        //Destroy(gameObject);
+        //gameObject.SetActive(false); // keep bullet alive to keep Coroutine running
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<SphereCollider>().enabled = false;
+        transform.parent = curExplosion.transform;
+        StartCoroutine("ScaleOverTime");
+    }
+
+    IEnumerator ScaleOverTime()
+    {
+        float t = 0;
+        while (t <= explosionDuration)
+        {
+            t += Time.deltaTime;
+            float scale = Mathf.Lerp(initialScale, endScale, t / explosionDuration);
+            curExplosion.transform.localScale = new Vector3(scale, scale, scale);
+            yield return null;
+        }
+        Destroy(curExplosion);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Towers/MortarTower.cs
+++ Assets/Scripts/Towers/MortarTower.cs
@@ -5,11 +5,11 @@ public class MortarTower : MonoBehaviour
 {
 
     public GameObject bullet;
-    public float bulletSpeed = 1f;
-    public float fireRate = 1f;
-    public float lifeTime = 5f;
-    public float fireRadius = 5f;
-    public float lobAmount = 1f;
+    //public float bulletSpeed = 1f;
+    public float fireRate = .3f;
+    public float lifeTime = 8f;
+    public float fireRadius = 8f;
+    public float lobAmount = 10f;
 
     // Use this for initialization
     void Start()
@@ -33,8 +33,10 @@ public class MortarTower : MonoBehaviour
         var newBullet = Instantiate(bullet, transform.position, bullet.transform.rotation) as GameObject;
 
         var verticalForce = Vector3.up * lobAmount;
+        var horizontalForce = (target.transform.position - transform.position).normalized;
+        float speed = lobAmount / -Physics.gravity.y;
 
-        newBullet.GetComponent<Rigidbody>().AddForce((target.transform.position - transform.position).normalized * bulletSpeed, ForceMode.VelocityChange);
-        Destroy(newBullet, lifeTime);
+        newBullet.GetComponent<Rigidbody>().AddForce((verticalForce + horizontalForce) * speed, ForceMode.VelocityChange);
+        //Destroy(newBullet, lifeTime);
     }
 }
